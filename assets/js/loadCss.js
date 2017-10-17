1f537e3d854a1d017fddc8a92a9af2cfdcad0a0c
Angelo <angeloparziale94@gmail.com>
2017-10-18 00:36:29 +0200

    nuove regole e funzionalità

--- assets/js/loadCss.js
+++ assets/js/loadCss.js
@@ -1,3 +1,28 @@
 /**
- * Created by angelo on 17/10/17.
+ * Created by aparzi on 17/10/17.
  */
+
+var number = Math.floor((Math.random() * 5) + 1);
+
+switch (number) {
+
+    case 1:
+        $('head').append('<link rel="stylesheet" type="text/css" href="assets/css/color/orange.css">');
+        break;
+
+    case 2:
+        $('head').append('<link rel="stylesheet" type="text/css" href="assets/css/color/green.css">');
+        break;
+
+    case 3:
+        $('head').append('<link rel="stylesheet" type="text/css" href="assets/css/color/lime.css">');
+        break;
+
+    case 4:
+        $('head').append('<link rel="stylesheet" type="text/css" href="assets/css/color/purple.css">');
+        break;
+
+    case 5:
+        $('head').append('<link rel="stylesheet" type="text/css" href="assets/css/color/red.css">');
+        break;
+}
