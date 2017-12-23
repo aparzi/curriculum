e6cdbcbf4c3764b3acb3f4dc874d80b374ee51cf
Angelo <angelo@angelo-pc-fisso>
2017-12-23 15:00:17 +0100

    nuova traduzione del sito

---
+++ resource/lang_eng.js
@@ -0,0 +1,46 @@
+/***************************** NAVBAR **************************************/
+var ABOUT = 'about'
+var WORKS = 'works'
+var EDUCATION = 'education'
+var CONTACT_ME = 'contact'
+
+/************************** INTRODUZIONE ********************************/
+var INTRODUCTION = "My name is Angelo Parziale and I'm a web and mobile developer. As for web work development work as a front-end, using javascript stack technology,"+
+                    "which back-end, in this case I use a lot of php framework or php object-oriented, in some cases I also used javascript server side (Nodejs)."+
+                    "For mobile worlds, I develop native apps using the ionic2 framework but also native apps developing in android."+
+                    "About my character that to say ... I'm a guy who loves to relate to people so you can contact me not only for work issues but also to build a new relationship of friendship. 😃";
+
+/************************* EQUIPAGGIAMENTO PERSONALE ************************************/
+var MY_EQUIPMENT = "PERSONAL EQUIPMENT"
+var JETBRAINS = "As a development environment (IDE) I use only JetBrains products, I was fortunate to use their products and I can't longer do without."+
+                "My favorites are <b>PhpStorm</b>, <b>IntelliJ Idea</b> and <b>Android Studio</b>.";
+var GIT = "Each of my projects begins with the command: <code>git init</code>. I use git from command line and its <b>github</b> web interface.";
+var LINUX = "For four years now I use only the Linux operating system 🐧, I work with the <b>Ubuntu</b> distro. In my free time I enjoy testing all the other Linux distributions.";
+
+/************************* WORK EXPERIENCE ************************************/
+var WORK_EXPERIENCE = "<i class=\"fa fa-suitcase\"> </i>&nbsp;Work Experience";
+var CONTACT = "Contact";
+var PROGRAMMING_LANGUAGE = "Languages &amp; Frameworks";
+var TITLE_XEOS = "<a target='_blank' href='http://xeos.it/'><span class='custom-span' style='color: #000;'>Xe</span><span class='custom-span' style='color: #ff0000'>os</span></a>";
+var TITLE_APP_UNIMOL = "<img style=\"margin-top: -7px;\" src=\"assets/images/projects/myunimol.png\" width=\"32\" height=\"32\" alt=\"logo myUnimol\" />&nbsp;MyUnimol";
+var TITLE_DATASOUNDS = "Datasounds&nbsp;<img style=\"margin-top: -14px;\" src=\"assets/images/projects/datasounds.png\" width=\"32\" height=\"32\" alt=\"logo myUnimol\" />";
+var TITLE_GRS_2017 = "<img style=\"margin-top: -7px;\" src=\"assets/images/projects/unimol.png\" width=\"32\" height=\"32\" alt=\"logo unimol\" />&nbsp;Grs 2017";
+var TITLE_SANER_2018 = "SANER 2018&nbsp;<img style=\"margin-top: -7px;\" src=\"assets/images/projects/logoSaner.png\" width=\"32\" height=\"32\" alt=\"logo saner\" />";
+
+var DESCRIPTION_XEOS = "I collaborate remotely with <a style='color: red;' target='_blank' href='http://www.xeos.it'>Xeos.it</a> as Software developer. In the company I work to developed services of type Rest or Soap services, in addition to the implementation phase I also take care of the testing phase."+
+                       "As a programming language we use Java8 together with the Spring boot framework and the Maven dependency manager.";
+var DESCRIPTION_APP_UNIMOL = "Along with a team of friends and colleagues, I am following the development and maintenance of <a style='color: red;' href='https://myunimol.it/Login.jsp' target='_blank'>MyUnimol</a>."+
+    "An application to manage the university career of students at The University of Molise."+
+    "MyUnimol, also available for <a style='color: red;' href='https://play.google.com/store/apps/details?id=rocks.teammolise.myunimol' target='_blank'>Android</a> and <a style='color: red;' href='https://itunes.apple.com/us/app/id1123469490' target='_blank'>iOS</a>.";
+var DESCRIPTION_GRS = "I made the website for GRS 2017 (Days of Scientific Research) an event that was held at the University of Molise specifically at the Pesche office. I did the work using technologies such as bootstrap, html and javascript.";
+var DESCRIPTION_DATASOUNDS = "I callaborated for just over a year with Molise's spin-off, <a style='color: red; cursor: pointer;' href='https://datasounds.io/' target='_blank'>Datasounds</a>. I have done the role of developer, specifically I was dealing with the frontend using the javascript  technology. The team I worked with was generally formed by 3/4 people, plus the senior.";
+var DESCRIPTION_SANER_2018 = "I developed the site of Saner 2018, an international conference on software maintenance, using web technologies such as php and javascript.";
+var NON_TERMINATO = "in progress";
+
+/**************************** EDUCTION ****************************************/
+var TITLE_DIPLOMA = "ELECTRONICS TECHNICIAN";
+var DESCRIPTION_DIPLOMA = "Graduation of the degree in electronic operator at the Istituto Professionale Industria e Artigianato located in Campobasso (CB).";
+var TITLE_CERTIFICATE = "INFORMATIC OPERATOR";
+var DESCRIPTION_CERTIFICATE = "Completion of certificate in computer operator. During the course I learned advanced knowledge of the entire Microsoft Office suite.";
+var TITLE_UNIVERSITY = "UNIVERSITY OF MOLISE";
+var DESCRIPTION_UNIVERSITY = "In the year 2014 I joined Molise University and currently I'm continuing my IT course.";
